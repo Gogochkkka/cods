4968f32afb1572556997aaeaa0bdb1d5fca5543e
Gogochkkka <gabagabrielyan@gmail.com>
2023-09-14 17:31:34 +0300

    Готовый вариант проекта уже точно

--- te.py
+++ te.py
@@ -1,13 +1,21 @@
-print('Прошу, введите коэффициенты квадратичного уравнения:')
-a, b, c = map(int, input().split())
-D = b**2 - 4 * a * c
-if D < 0:
-    print('Корней во множетсве R отсутвуют')
+def f(x):
+    return x.isupper() or x.islower()
+
+
+print('Прошу, введите коэффициенты квадратичного уравнения на следующей строке:')
+a, b, c = map(str, input().split())
+if not f(a) and not f(b) and not f(c):
+    a, b, c = float(a), float(b), float(c)
+    D = b**2 - 4 * a * c
+    if D < 0:
+        print('Корней во множетсве R отсутвует')
+    else:
+        x_one = (-b + D**0.5)/(2*a)
+        x_two = (-b - D**0.5)/(2*a)
+        if x_one == int(x_one):
+            x_one = int(x_one)
+        if x_two == int(x_two):
+            x_two = int(x_two)
+        print(f'''Корни: {x_one}, {x_two}''')
 else:
-    x_one = (-b + D**0.5)/(2*a)
-    x_two = (-b - D**0.5)/(2*a)
-    if x_one == int(x_one):
-        x_one = int(x_one)
-    if x_two == int(x_two):
-        x_two = int(x_two)
-    print(f'''Корни: {x_one}, {x_two}''')
+    print('Введены некорректные данные')
